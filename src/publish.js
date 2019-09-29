4e996d4b4db1a312364c7312e4ba49c4821efd9b
Jarvan <liushileijarvan@gmail.com>
2019-09-29 17:13:44 +0800

    支持 publish 其他配置

--- src/publish.js
+++ src/publish.js
@@ -43,7 +43,7 @@ module.exports = (registry) => {
 			if (ynpmConfig.user && ynpmConfig.sshk && data) {
 				console.log('Aviable: Pass Validation, Start to Publish...')
 				let userconfig = getRcFile('ynpm')
-				var arg_publish_inner = `npm --registry=${HOST_REGISTRY} --userconfig=${userconfig} publish`;
+				const arg_publish_inner = `npm --registry=${HOST_REGISTRY} --userconfig=${userconfig} publish ` + argvs.slice(3).join(' ');
 				spinner.text = 'Publishing your package in Yonyou Local Area Net';
 				try {
 					let publish_result = yield Exec(arg_publish_inner);

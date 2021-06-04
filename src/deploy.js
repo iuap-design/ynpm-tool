b41b8ffce0f7b8795ab890a8cae2a965100c8062
liushld <liushld@yonyou.com>
2021-06-04 14:35:16 +0800

    提交更新

--- src/deploy.js
+++ src/deploy.js
@@ -6,15 +6,16 @@ const chalk = require('chalk');
 const fs = require('fs');
 const fetch = require('node-fetch');
 const formData = require('form-data');
-const {YNPM_SERVER_DEPLOY} = require('./utils');
+const {YNPM_SERVER_DEPLOY, getRc} = require('./utils');
 
-function deploy(path, environment, project) {
+function deploy(path, environment, project, sshk) {
 	try {
 		let form = new formData();
 		if (fs.existsSync(path)) {
 			form.append("file", fs.createReadStream(path));
 			form.append("environment", environment);
 			form.append("project", project);
+			form.append("sshk", sshk);
 			console.log(chalk.green('Deploy start..., Please wait upload!'));
 			return fetch(YNPM_SERVER_DEPLOY + '/ynpm/deploy', {method: 'post', body: form})
 				.then(res => res.json())
@@ -25,7 +26,7 @@ function deploy(path, environment, project) {
 						console.log(chalk.green('CDN URL: ' + res.data));
 					} else {
 						console.log('\n')
-						console.log(res.msg);
+						console.log(chalk.red('[ERROR]:' + res.data));
 					}
 				}).catch(err => {
 					console.log('\n');
@@ -46,15 +47,17 @@ module.exports = () => {
 	const spinner = ora().start();
 	spinner.color = 'green';
 	co(function* () {
+		const ynpmConfig = getRc("ynpm");
 		const argvs = process.argv.slice(3);
 		const environment = argvs[1];
 		const project = argvs[0];
 		const path = argvs[2];
-		if (environment && project && path) {
-			yield deploy(path, environment, project);
+		const sshk = argvs[3] || ynpmConfig.sshk;
+		if (environment && project && path && sshk) {
+			yield deploy(path, environment, project, sshk);
 			spinner.stop();
 		} else {
-			console.error(chalk.red('\n' + 'Please enter the package name'));
+			console.error(chalk.red('\n' + 'Please input related content'));
 		}
 		process.exit(0);
 	}).catch(err => {
